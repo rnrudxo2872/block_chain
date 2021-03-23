91b75f09c060073d66acfb93234745dcfbe09e25
unknown <rnrudxo2872@naver.com>
2021-03-23 23:34:26 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestaps) {
         this.index = index;
@@ -7,7 +10,16 @@ class Block {
         this.timestaps = timestaps;
     }
 }
+Block.calculateHash = (index, previousHash, data, timestaps) => {
+    return CryptoJS.SHA256(index + previousHash + data + timestaps).toString(); //block의 해시값 계산, static으로 블럭생성전 해시값 계산.
+};
 const genesisBlock = new Block(0, "231454", "", "I'm first block!", 123); // 초기 블럭 생성
 let BlockChain = [genesisBlock]; // 첫 블럭을 chain에 넣는다.
+const getBlockChain = BlockChain; // 블럭 배열을 반환받아 블럭체인을 알아낸다.
+const getLatestBlock = BlockChain[BlockChain.length - 1]; // 마지막으로 업데이트된 블럭을 얻어낸다.
+const getNewTimeStemp = Math.round(new Date().getTime() / 1000); // timestemp를 구하는 상수. 반올림해서 정수 얻음.
+//getTime = 1970.01.01기준 이후 흘러온 milisec. 
 console.log(BlockChain);
+console.log(getLatestBlock);
+console.log(getNewTimeStemp);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import *as CryptoJS from 'crypto-js';
+
 class Block{
     public index: number;
     public hash: string;
     public previousHash: string;
     public data : string;
     public timestaps : number;
+
+    static calculateHash = (
+        index: number,
+        previousHash: string,
+        data : string,
+        timestaps : number):string => {
+        return CryptoJS.SHA256(index+previousHash+data+timestaps).toString(); //block의 해시값 계산, static으로 블럭생성전 해시값 계산.
+    }
     
     constructor(
         index: number,
@@ -20,7 +30,15 @@ class Block{
 }
 
 const genesisBlock:Block = new Block(0,"231454","","I'm first block!",123); // 초기 블럭 생성
-let BlockChain: [Block] = [genesisBlock]; // 첫 블럭을 chain에 넣는다.
+let BlockChain: Block[] = [genesisBlock]; // 첫 블럭을 chain에 넣는다.
 
+const getBlockChain : Block[] = BlockChain; // 블럭 배열을 반환받아 블럭체인을 알아낸다.
+const getLatestBlock : Block = BlockChain[BlockChain.length - 1]; // 마지막으로 업데이트된 블럭을 얻어낸다.
+const getNewTimeStemp : number = Math.round(new Date().getTime() / 1000); // timestemp를 구하는 상수. 반올림해서 정수 얻음.
+                            //getTime = 1970.01.01기준 이후 흘러온 milisec. 
 console.log(BlockChain);
+console.log(getLatestBlock);
+console.log(getNewTimeStemp);
+
+
 
